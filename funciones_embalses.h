307e5cf0a9e3a8b3bfba52847bebca43f7c5733a
C-monegro <celiamonegro@hotmail.com>
2025-04-21 22:38:09 +0200

    Update funciones_embalses.h

--- funciones_embalses.h
+++ funciones_embalses.h
@@ -1,1 +1,22 @@
+typedef struct {
+	char cuenca_hidrografica[50];
+	char embalse_nombre[50];
+	int mes;
+	float dosmildoce;
+	float dosmiltrece;
+	float dosmilcatorce;
+	float dosmilquince;
+	float dosmildieciseis;
+	float dosmildiecisiete;
+	float dosmildieciocho;
+	float dosmildiecinueve;
+	float dosmilveinte;
+	float dosmilveintiuno;
+
+	char embalse_nom[50];
+	float etotal;
+	
+} linea;
+
+float porcentaje_embalse (int anyo, int mes, int num_cuenca, int vector2[], int num_embalse, int i, linea vector[], linea vector1[]);
 
