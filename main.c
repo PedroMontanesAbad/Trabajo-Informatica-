b15e871ee7d7c964d68ea1f8d3fa136530fcc694
C-monegro <celiamonegro@hotmail.com>
2025-04-23 00:07:05 +0200

    Update main.c

--- main.c
+++ main.c
@@ -5,15 +5,15 @@
 
 int main(){
 	
-	int f=0,j=0, k=0, i;
+	int f=0,j=0, k=0, b=0, c,i;
 
 	int num_embalse, anyo, mes,num_cuenca, posiciones_cuencas[15] ;
 
-	float porcentaje, fecha, dato;
+	float porcentaje, capacidad_max, capacidad_actual;
 
 	linea cuenca[4236], embalse [353];
 	
-	//leemos los ficheros	
+	//abrimos ficheros
 	
 	FILE *tabla;
 	tabla = fopen("texto_proyecto.txt","r");
@@ -31,30 +31,32 @@ int main(){
 	return -1;
 	}
 
+	//leo el primer fichero	
+	
 	while (fscanf(tabla, "%s\t%s\t%i\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\n", cuenca[j].cuenca_hidrografica , cuenca[j].embalse_nombre , &cuenca[j].mes , &cuenca[j].dosmildoce, &cuenca[j].dosmiltrece, &cuenca[j].dosmilcatorce, &cuenca[j].dosmilquince, &cuenca[j].dosmildieciseis, &cuenca[j].dosmildiecisiete, &cuenca[j].dosmildieciocho,  &cuenca[j].dosmildiecinueve,  &cuenca[j].dosmilveinte,  &cuenca[j].dosmilveintiuno ) != EOF)
 	{
+	
 	//printf("Linea %i:\n", j);
-	//printf("%s\t%s\t%i\t%f\t%f\t%f\t%f\t%f\t%f\t%f\n", cuenca[j].cuenca_hidrografica , cuenca[j].embalse_nombre , cuenca[j].mes , cuenca[j].dosmildoce, cuenca[j].dosmiltrece, cuenca[j].dosmilcatorce, cuenca[j].dosmilquince, cuenca[j].dosmildieciseis, cuenca[j].dosmildiecisiete, cuenca[j].dosmildieciocho,  cuenca[j].dosmildiecinueve,  cuenca[j].dosmilveinte,  cuenca[j].dosmilveintiuno );	
+	//printf("%s\t%s\t%i\t%f\t%f\t%f\t%f\t%f\t%f\t%f\n", cuenca[j].cuenca_hidrografica , cuenca[j].embalse_nombre , cuenca[j].mes , cuenca[j].dosmildoce, cuenca[j].dosmiltrece, cuenca[j].dosmilcatorce, cuenca[j].dosmilquince, cuenca[j].dosmildieciseis, cuenca[j].dosmildiecisiete, cuenca[j].dosmildieciocho,  cuenca[j].dosmildiecinueve,  cuenca[j].dosmilveinte,  cuenca[j].dosmilveintiuno );
+	
 	j++;
 	}
+	
 	// las comparamos para no repetir
 	for (i=0;i<j;i++){
+	
 	if (strcmp(cuenca[i].cuenca_hidrografica, cuenca[i-1].cuenca_hidrografica) != 0){
 		posiciones_cuencas[f]=i;
 		f++;
 	}
-  }
+}
 
-while ( fscanf(lista, "%s\n%f\n", embalse[k].embalse_nom , &embalse[k].etotal) !=EOF )
-	{
-	//printf("%s\t%.2f\n", embalse[k].embalse_nom, embalse[k].etotal );
-	k++;	
-	}
 
-//Porcentaje en un mes concreto
-printf("Calculo del porcentaje:\n\n ");
 
-//cuenca
+
+//Nombres cuencas numeradas:
+nombres_cuencas(j,cuenca);
+//num cuenca
 do{
 	printf("\nIntroduce el numero de la cuenca: ");
 	scanf("%i", &num_cuenca);
@@ -67,25 +69,47 @@ do{
 
 while ((0 >= num_cuenca) || ( num_cuenca > 16 ));
 
-//embalse
+
+//Nombres de los embalses de una cuenca en concreto:
+
+nombres_cuencas_embalse( j, num_cuenca, cuenca, posiciones_cuencas, &c);
+
+//num embalse
+
 do{
 	printf("\nIntroduce el numero del embalse: " );	
 	
 	
 	scanf("%i", &num_embalse);
 
-	if((0 > num_embalse) || ( num_embalse > 353 )){
+	if((0 > num_embalse) || ( num_embalse > c )){
 		printf ("\nNumero no valido, repite:\n");
 	}
 
 }
-while ((0 > num_embalse) || ( num_embalse > 353 ));
+while ((0 > num_embalse) || ( num_embalse > c));
+
+system("cls");
 
 for(i=posiciones_cuencas[num_cuenca-1]+(num_embalse-1)*12;i<posiciones_cuencas[num_cuenca-1]+(num_embalse)*12;i++)
 	{}printf("Tu embalse es: %s\n",cuenca[i-1].embalse_nombre) ;
+	
+	
+	while ( fscanf(lista, "%s\t%f\n", embalse[k].embalse_nom , &embalse[k].etotal) !=EOF )
+	{
 
+	//printf("%s\t%.2f\n", embalse[k].embalse_nom, embalse[k].etotal );
+	
+	if  (strcmp(cuenca[i-1].embalse_nombre , embalse[k].embalse_nom) == 0){
+		b = k+1;
+	}
+	k++;	
+	}
+
+	
+	
 printf("\n\nAhora vamos a seleccionar la fecha: ");
-//anyo
+
 do{
 	printf("\nIntroduce el anyo deseado 2012-2021: ");
 		scanf("%i", &anyo);
@@ -95,7 +119,7 @@ do{
 }
 
 while ((2012 > anyo) || ( anyo > 2021 ));
-//mes
+
 do{
 	printf("\nIntroduce el mes deseado: ");
 		scanf("%i", &mes);
@@ -105,19 +129,23 @@ do{
 }
 while ((1 > mes) || ( mes > 12 ));
 
-porcentaje = porcentaje_embalse (anyo, mes, num_cuenca, posiciones_cuencas, num_embalse, i, embalse, cuenca);
-printf("El porcentaje de agua del embalse %s de la cuenca %s es %f, en el mes %i del anyo %i", cuenca[posiciones_cuencas[num_cuenca-1]+(num_embalse-1)*12].embalse_nombre, cuenca[posiciones_cuencas[num_cuenca-1]+(num_embalse-1)*12].cuenca_hidrografica, porcentaje, mes, anyo);
+porcentaje = porcentaje_embalse (anyo, mes, num_cuenca, posiciones_cuencas, b, num_embalse, cuenca, embalse);
+printf("El embalse %s de la cuenca %s tiene una capadidad del %.2f%%, en el mes %i del anyo %i", cuenca[posiciones_cuencas[num_cuenca-1]+(num_embalse-1)*12].embalse_nombre, cuenca[posiciones_cuencas[num_cuenca-1]+(num_embalse-1)*12].cuenca_hidrografica, porcentaje, mes, anyo);
 
-//printf("\n%.2f, %.2f",fecha,dato);
-	
-		if (porcentaje<10){
-			printf("Periodo de sequia.");
+//printf("\n%.2f, %.2f",capacidad_max,capacidad_actual);
+
+		if ((50<porcentaje) && (porcentaje<70)){
+			printf("\nPeriodo de sequia leve.");
+		}		
+		else if ((30<porcentaje) && (porcentaje<50)){
+			printf("\nPeriodo de sequia moderada.");
 		}
-	
-
+		else if (porcentaje<30){
+			printf("\nPeriodo de sequia grave.");
+		}
+		
+		
 
 fclose(tabla);
 fclose(lista);
 }
-
-	
