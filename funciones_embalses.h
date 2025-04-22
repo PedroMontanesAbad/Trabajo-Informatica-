014a10d5927cccfeafa5ff449042be7a8459ef51
C-monegro <celiamonegro@hotmail.com>
2025-04-23 00:06:12 +0200

    Update funciones_embalses.h

--- funciones_embalses.h
+++ funciones_embalses.h
@@ -1,4 +1,4 @@
-typedef struct {
+ttypedef struct {
 	char cuenca_hidrografica[50];
 	char embalse_nombre[50];
 	int mes;
@@ -18,5 +18,9 @@ typedef struct {
 	
 } linea;
 
-float porcentaje_embalse (int anyo, int mes, int num_cuenca, int vector2[], int num_embalse, int i, linea vector[], linea vector1[]);
 
+void nombres_cuencas(int j,linea cuenca[]);
+
+void nombres_cuencas_embalse(int j, int num_cuenca, linea vector[], int vector2[],int *c);
+
+float porcentaje_embalse (int anyo, int mes, int num_cuenca, int vector2[], int b, int num_embalse, linea vector[], linea vector1[]);
