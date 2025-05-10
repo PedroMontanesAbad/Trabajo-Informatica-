fa76af3241b290c7106abdf8ce384ddc56d2c851
ElenaSU13 <sanchezunamunzagaelena@gmail.com>
2025-05-10 20:18:16 +0200

    Update lib.h

--- lib.h
+++ lib.h
@@ -52,5 +52,25 @@ float media_anyo(int j,int anyo, linea vector[]);
 float media_anyo_embalse(int j,int anyo, int num_cuenca, int num_embalse, linea vector[], int vector2[]);
 float max_y_min_embalse(int j,int anyo, int num_cuenca, int num_embalse, linea vector[], int vector2[], int vector3[]);
 
+//Funciones_comparacion
+void nombres_cuencas_E(int vb, frase cuenca[]);
+int seleccion_cuenca_1(int num_cuenca, char C_cuenca[]);
+void nombreseleccion_embalse_1(int num_cuenca,frase cuenca[], int* num_embalse, int* l_r_embalse);
+int seleccion_cuenca_2(int num_cuenca2, char C_cuenca2[]);
+void nombreseleccion_embalse_2(int num_cuenca2,frase cuenca[], int* num_embalse2, int* l_r_embalse2);
+int tipo_comparacion(int mes_anyo);
+void comparacion_anyo(int mes_anyo, int anyo, int l_r_embalse, int l_r_embalse2, frase cuenca[]);
+int seleccion_anyo(int anyo);
+void comparacion_mes(int mes_anyo, int anyo, int mes, int l_r_embalse, int l_r_embalse2, frase cuenca[]);
+int seleccion_mes(int mes);
+char *meses_nombre(int mes);
+void capacidad_max(int l_r_embalse, int l_r_embalse2, frase cuenca[],frase embalse[]);
+void superficie(int l_r_embalse, int l_r_embalse2, frase cuenca[], DatosComparacion comp[]);
+void tipo_embalse(DatosComparacion comp[],int l_r_embalse, int l_r_embalse2, frase cuenca[]);
+void uso_embalse(DatosComparacion comp[],int l_r_embalse, int l_r_embalse2, frase cuenca[]);
+char cambios_espacios_embalses_E(frase cuenca [], int l_r_embalse, int l_r_embalse2);
+char cambios_espacios_tipo_presa(DatosComparacion comp[], int l_r_embalse, int l_r_embalse2);
+//Funciones_comparacion
+
 #endif
 
