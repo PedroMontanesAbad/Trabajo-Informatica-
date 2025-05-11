db6ebe49961750e75cdf6fd056788ffb74958cef
ElenaSU13 <sanchezunamunzagaelena@gmail.com>
2025-05-11 17:20:32 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,25 +1,37 @@
 #include <stdio.h>
 #include <string.h>
 #include <float.h>
+#include <stdlib.h>
 #include "comparacion_embalse.h"
 
 
+
 int main(){
-	
-//variables
+	//variables
+
 int vb=0, vc=0, vco=0,i;//for y ficheros
 
 int num_cuenca,num_embalse,l_r_embalse;//embalse1
 int num_cuenca2,num_embalse2,l_r_embalse2;//embalse2
 int mes_anyo, anyo,mes;//fechas y meses
+char C_cuenca[2];
 	
-frase cuenca[4236], embalse[353]; 
-DatosComparacion comp[353];
+linea *cuenca = NULL;
+Embalse *embalse = NULL;
+DatosComparacion *comp= NULL;
 
+cuenca = (linea *)malloc(4236 * sizeof(linea));
+embalse = (Embalse *)malloc(353 * sizeof(Embalse));
+comp = (DatosComparacion *)malloc(353 * sizeof(DatosComparacion));
+
+if(cuenca==NULL||embalse==NULL||comp==NULL){
+	printf("Error al reservar memoria.\n");
+	return -1;
+}
 	
 //Fich
 FILE *base;
-base=fopen("dataset.csv","r");
+base=fopen("texto_proyecto.csv","r");
 		if(base==NULL){
 			printf("Error al abrir el fichero base.\n");
 			return -1;
@@ -44,22 +56,27 @@ fseek(base,90,SEEK_SET);
 	while (fscanf(base, "%[^,],%[^,],%i,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f\n", cuenca[vb].cuenca_hidrografica , cuenca[vb].embalse_nombre , &cuenca[vb].mes , &cuenca[vb].dosmildoce, &cuenca[vb].dosmiltrece, &cuenca[vb].dosmilcatorce, &cuenca[vb].dosmilquince, &cuenca[vb].dosmildieciseis, &cuenca[vb].dosmildiecisiete, &cuenca[vb].dosmildieciocho,  &cuenca[vb].dosmildiecinueve,  &cuenca[vb].dosmilveinte,  &cuenca[vb].dosmilveintiuno ) != EOF)
 	{
 	
+
+
     	vb++;
 	}
 while(fscanf(capacidad,"%s\t%f\n",embalse[vc].embalse_nom,&embalse[vc].etotal)!=EOF){
-	
+
 		vc++;
 	}
 while(fscanf(F_comparacion,"%99[^,],%f,%99[^,],%4[^,],%4[^,],%4[^,],%4[^,],%4[^,],%4[^,],%4[^,],%4[^,],%4[^\n]\n",comp[vco].nombre_embalse,&comp[vco].superficie,comp[vco].tipo_presa,comp[vco].uso_abastecimiento,comp[vco].uso_riego,comp[vco].uso_electricidad,comp[vco].uso_industrial,comp[vco].uso_pesca,comp[vco].uso_navegacion,comp[vco].uso_banyo,comp[vco].uso_picnic,comp[vco].uso_restaurante)!=EOF){
 	    vco++;
+	    
 	}
 
+
 do{
+
 //LISTA CUENCAS
 nombres_cuencas(vb,cuenca);
 
 //SELECCION CUENCA
-num_cuenca=seleccion_cuenca_1(num_cuenca);
+num_cuenca=seleccion_cuenca_1(num_cuenca, C_cuenca);
 
 //SELECCION EMBALSE
 nombreseleccion_embalse_1(num_cuenca,cuenca, &num_embalse, &l_r_embalse);
@@ -68,7 +85,7 @@ nombreseleccion_embalse_1(num_cuenca,cuenca, &num_embalse, &l_r_embalse);
 nombres_cuencas(vb,cuenca);
 
 //SELECCION 2 CUENCA:
-num_cuenca2=seleccion_cuenca_2(num_cuenca2);
+num_cuenca2=seleccion_cuenca_2(num_cuenca2,C_cuenca);
 
 //SELECCION 2 EMBALSE
 nombreseleccion_embalse_2(num_cuenca2,cuenca, &num_embalse2, &l_r_embalse2);
@@ -79,6 +96,7 @@ if(l_r_embalse==l_r_embalse2){
 
 }while(l_r_embalse==l_r_embalse2);
 
+
 cambios_espacios_embalses(cuenca,l_r_embalse,l_r_embalse2);
 cambios_espacios_tipo_presa (comp,l_r_embalse,l_r_embalse2);
 
@@ -86,18 +104,19 @@ cambios_espacios_tipo_presa (comp,l_r_embalse,l_r_embalse2);
 	printf("El segundo embalse elegido es %s\n", cuenca[l_r_embalse2-1].embalse_nombre);
 
 //TIPO COMPARACION
+
 mes_anyo=tipo_comparacion(mes_anyo);
 
 //COMPARACION SEGUN AÑO:
 if(mes_anyo==1){
-	anyo=seleccion_anyo(anyo);
+	anyo=seleccion_anyos(anyo);
 	comparacion_anyo(mes_anyo,anyo,l_r_embalse,l_r_embalse2,cuenca);
 }
 
 //COMPARACION SEGUN MES:
 if(mes_anyo==2){
-	anyo=seleccion_anyo(anyo);
-	mes=seleccion_mes(mes);
+	anyo=seleccion_anyos(anyo);
+	mes=seleccion_mese(mes);
     comparacion_mes(mes_anyo,anyo,mes,l_r_embalse,l_r_embalse2,cuenca);
 }
 
@@ -113,7 +132,13 @@ tipo_embalse(comp,l_r_embalse,l_r_embalse2,cuenca);
 //USO
 uso_embalse(comp,l_r_embalse,l_r_embalse2,cuenca);
 
+free(cuenca);
+free(embalse),
+free(comp);
 fclose(base);
 fclose(capacidad);
 fclose(F_comparacion);
+
+
+return 0;
 }
